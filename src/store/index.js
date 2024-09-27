7623d9418584bb22bec9e556affd40f55e5c13db
mqy <1876797770@qq.com>
2024-09-27 14:10:59 +0800

    token & id 存储在本地

--- src/store/index.js
+++ src/store/index.js
@@ -4,16 +4,25 @@ import axios from 'axios';
 const store = createStore({
     state() {
         return {
-            token: null,
-            clientId: null, // 添加 clientId 状态
+            token: localStorage.getItem('token') || null, // 从 LocalStorage 获取 token
+            clientId: localStorage.getItem('clientId') || null, // 从 LocalStorage 获取 clientId
+
         };
     },
     mutations: {
         setToken(state, token) {
             state.token = token;
+            localStorage.setItem('token', token); // 保存 token 到 LocalStorage
         },
-        setClientId(state, clientId) { // 添加 setClientId mutation
+        setClientId(state, clientId) {
             state.clientId = clientId;
+            localStorage.setItem('clientId', clientId); // 保存 clientId 到 LocalStorage
+        },
+        clearAuthData(state) {
+            state.token = null;
+            state.clientId = null;
+            localStorage.removeItem('token'); // 移除 token
+            localStorage.removeItem('clientId'); // 移除 clientId
         },
     },
     actions: {
@@ -21,8 +30,8 @@ const store = createStore({
             try {
                 const response = await axios.post('/auth', account);
                 if (response.data.code === 200) {
+                    commit('setClientId', account.clientId);
                     commit('setToken', response.data.data);
-                    commit('setClientId', account.clientId); // 存储 clientId
                     return true;
                 } else {
                     alert(response.data.info);
@@ -32,6 +41,9 @@ const store = createStore({
                 console.error('登录失败:', error);
                 return false;
             }
+        },
+        logout({ commit }) {
+            commit('clearAuthData'); // 清除 token 和 clientId
         },
     },
 });
